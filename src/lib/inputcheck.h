0ca08762fdf9bbbf96ac6229f8554dc20514c3a6
Малышев Владимир Александрович <ip014s19@bwp05.csc.local>
2021-03-10 18:07:41 +0700

    Created .h file

---
+++ src/lib/inputcheck.h
@@ -0,0 +1,7 @@
+#ifndef FIGURECHECK_H_
+#define FIGURECHECK_H_
+
+int inputcheck(char st[], int borderfigure, int dlina, int index);
+
+#endif
+
--- src/main.c
+++ src/main.c
@@ -1,39 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "lib/inputcheck.h"
 
-int inputcheck(char st[], int borderfigure, int dlina, int index)
-{
-    int borderx = 0;
-    int point = 0;
-    for (int i = borderfigure + 1; i < dlina; i++) {
-        if ((st[i] >= '0') && (st[i] <= '9')) {
-            continue;
-        } else {
-            if ((st[i] == ',') || ((st[i] == ')') && (index == 3))) {
-                borderx = i;
-                break;
-            }
-            if ((st[i] == '.') && (point == 0)) {
-                point++;
-                continue;
-            } else {
-                if (index == 1)
-                    printf("coord x wrong\n");
-                if (index == 2)
-                    printf("coor y wrong\n");
-                if (index == 3)
-                    printf("radius wrong\n");
-                while ((st[i] != ',') && (st[i] != ')')) {
-                    i++;
-                }
-                borderx = i;
-                break;
-            }
-        }
-    }
-    return borderx;
-}
 
 int main()
 {
